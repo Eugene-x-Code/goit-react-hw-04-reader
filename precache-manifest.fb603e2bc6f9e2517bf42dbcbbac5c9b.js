e2b2e77e2b0bf5e78bc626e09c4f5407cd76ded3
Eugene-x-Code <evgeni.iv.xy@gmail.com>
2019-12-06 20:31:27 +0200

    Updates

---
+++ precache-manifest.fb603e2bc6f9e2517bf42dbcbbac5c9b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "154b4caaeb27a6e9f1304003e2c9f1cb",
+    "url": "/goit-react-hw-04-reader/index.html"
+  },
+  {
+    "revision": "82cfc9e72d90aba3cec7",
+    "url": "/goit-react-hw-04-reader/static/css/main.780f258a.chunk.css"
+  },
+  {
+    "revision": "76f1262aab9c643e53aa",
+    "url": "/goit-react-hw-04-reader/static/js/2.0df8a446.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/goit-react-hw-04-reader/static/js/2.0df8a446.chunk.js.LICENSE"
+  },
+  {
+    "revision": "82cfc9e72d90aba3cec7",
+    "url": "/goit-react-hw-04-reader/static/js/main.82a545ca.chunk.js"
+  },
+  {
+    "revision": "fbe01e1d38aef890d8dd",
+    "url": "/goit-react-hw-04-reader/static/js/runtime-main.bc6aa647.js"
+  }
+]);
